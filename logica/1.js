2d181e6d95cf4a6260e0429c8577091b0ea2ba76
jlarata <ariel@cfee.gob.ar>
2024-05-08 12:06:07 -0300

    Primer ejercicio de lógica completo.

--- logica/1.js
+++ logica/1.js
@@ -1,4 +1,7 @@
-// Debes escribir un programa que encuentre la frecuencia con que aparecen los distintos caracteres (letras y números) dentro de una cadena de texto. El resultado se devuelve una matriz de objetos. Cada uno de estos objetos tiene 2 campos: carácter (caracter) y número de veces que aparece (veces). La matriz estará ordenada por el campo carácter. No se diferencia entre mayúsculas y minúsculas ni entre vocales acentuadas.
+// Debes escribir un programa que encuentre la frecuencia con que aparecen los distintos caracteres (letras y números)
+// dentro de una cadena de texto. El resultado se devuelve una matriz de objetos. Cada uno de estos objetos tiene 2 campos:
+// carácter (caracter) y número de veces que aparece (veces). La matriz estará ordenada por el campo carácter. No se diferencia 
+//entre mayúsculas y minúsculas ni entre vocales acentuadas.
 
 // Ejemplo:
 
@@ -17,6 +20,49 @@
 // {car: 's', veces: 1}
 // {car: 'y', veces: 2}
 
-const contarCar = value => {
-  return
+
+//Modificar Esta Variable con la cadena de texto a Analizar
+var stringARevisar = ("Hoy ya es día 10");
+
+const creaYOrdenaArray = stringAConvertir => {
+  //Ordenamiento alfanumérico previo a la construcción de objetos
+  var arrayDeChars = stringAConvertir.split('');
+  arrayDeChars.sort();
+  return arrayDeChars;
 }
+
+const frecuenciaCaracter = arrayOrdenado => {
+  //Crea una lista
+  var chars = [];
+  //Y por cada elemento del array de chars
+  for (var i=0; i<arrayOrdenado.length;i++)
+    {
+      //Descarta los espacios porque en el ejemplo estaban descartados (para volver a incluirlos, eliminar este condicional y listo)
+      if (arrayOrdenado[i] != " ")
+        {
+          //crea un objeto con  1) la propiedad  car y le asigno valor del char, y 2) propiedad numérica cant, asigndo 1.
+          var newChar = {car : arrayOrdenado[i], veces: 1}
+      
+          //Luego, si no hay ya un objeto en la lista con la misma propiedad "car", pusheo 
+          if (!chars.some(e => e.car === arrayOrdenado[i]))
+            {     
+              chars.push(newChar);
+            }
+          //Caso contrario en el elemento en cuestión sumo 1 unidad a pripiedad cant
+          else
+            {
+              var objIndex = chars.findIndex(e => e.car == arrayOrdenado[i]);
+              chars[objIndex].veces ++;
+            }
+        }
+
+    }
+    
+  return [chars]
+}
+
+var arrayDeChars = creaYOrdenaArray(stringARevisar.toLocaleLowerCase());
+var arrayOrdenado = frecuenciaCaracter(arrayDeChars);
+console.log(arrayOrdenado)
+
+
